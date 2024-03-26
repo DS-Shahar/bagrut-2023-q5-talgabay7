2656fa29f6c412670ada4af2f5054af5f1babd10
talgabay7 <126403499+talgabay7@users.noreply.github.com>
2024-03-26 13:09:25 +0200

    Update OrderedList.java

--- src/main/java/OrderedList.java
+++ src/main/java/OrderedList.java
@@ -4,10 +4,34 @@ public class OrderedList {
 	private Node<NumCount> lst;
 
 	public void insertNum(int x) {
+		Node<NumCount> p=new Node<>(null,lst);
+		lst=p;
+		boolean flag=false;
+		Node<NumCount> temp;
+		while(lst.hasNext()&&!flag){
+			if(lst.getNext().getValue().getNum()==x){
+				lst.getNext().getValue().setCount(lst.getNext().getValue().getCount()+1);
+				flag=true;
+			}
+			else if(lst.getNext().getValue().getNum()>x){
+				temp=new Node<NumCount>(new NumCount(x,1),lst.getNext());
+				lst.setNext(temp);
+				flag=true;
+			}
+			lst=lst.getNext();
+		}
 		System.out.println("insert " + x);
 	}
 
-	public int valueN(int n) {		
+	public int valueN(int n) {
+		int count=0;
+		Node<NumCount> p=lst;
+		while(p!=null){
+			count+=p.getValue().getCount();
+			if(count>=n)
+				return p.getValue().getNum();
+			p=p.getNext();
+		}
 		return -1;
 	}
 
